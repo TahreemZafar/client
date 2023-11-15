20b36b337b5be0fc2a6054c3644eb7dfc2a45b23
Tahreem Zafar <tahreem100m@gmail.com>
2023-11-15 12:55:28 +0500

    server

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://back-estate.netlify.app/',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
