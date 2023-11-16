54e915e5e6f4175660cefeb80c91a626af43bccf
Tahreem Zafar <tahreem100m@gmail.com>
2023-11-16 10:50:12 +0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://mern-estate-teal.vercel.app',
-        secure: false,
+        changeOrigin: true,
       },
     },
   },
